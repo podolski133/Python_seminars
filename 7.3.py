14bdc5e068f3b792edc5d95cf793bdd6593def24
OlegZavodtsov <podolski133@gmail.com>
2022-07-16 17:16:38 +0300

    add 7,7.1,7.2,7.3, change Readme

---
+++ 7.3.py
@@ -0,0 +1,32 @@
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+# между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+def input_numbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Ты ошибся. Вводить надо целые числа!")
+    return a
+
+
+def calcul_length_segment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = input_numbers(2)
+print("Введите координаты точки В")
+pointB = input_numbers(2)
+
+print(f"Длина отрезка: {format(calcul_length_segment(pointA, pointB), '.2f')}")
