08fa8a76e2b686e7e6a444666e9a526ab9744f2f
OlegZavodtsov <podolski133@gmail.com>
2022-07-19 20:11:53 +0300

    change 7

--- 7.py
+++ 7.py
@@ -1,8 +1,7 @@
 # Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
-x = 0
-y = 1
-left = not (x or y)
-right = not x and not y
-statement = left == right
-print (statement)
+rezult = True
+for x in True, False:
+    for y in True, False:
+        for z in True, False:
+            print(f"{x = } {y = } {z = }     RESULT: {not(x or y or z) == (not x and not y and not z)}")
